2d92385bcd91b0ccda99c2c51f78a24176f55335
Vicente-S-code <vicente.orellana1512@gmail.com>
2025-09-25 11:23:49 -0300

    mi primera clase
    (-.-)

---
+++ clase01u2.py
@@ -0,0 +1,12 @@
+class participante:
+    def __init__(self, rut: str, nombre:str, edad:int):
+        self._rut: str = rut
+        self._nombre: str = nombre
+        self._edad: int = edad
+    def presentarse(self)-> str:
+        return f"hola mi nombre es {self._nombre} y mi edad es {self._edad}"
+    
+
+participante1: participante = participante("22283696-4", "Vicente Sagredo", 18)
+print(participante1.presentarse())
+
